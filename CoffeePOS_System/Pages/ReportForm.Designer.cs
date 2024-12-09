80be9510d6bcf875ec6b31b04fadf226ac998a8f
MeanLesss <68323176+MeanLesss@users.noreply.github.com>
2024-12-09 17:03:38 +0700

    asdf

--- CoffeePOS_System/Pages/ReportForm.Designer.cs
+++ CoffeePOS_System/Pages/ReportForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridViewReport = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewReport)).BeginInit();
             this.SuspendLayout();
             // 
@@ -56,11 +57,23 @@
             this.dataGridViewReport.TabIndex = 1;
             this.dataGridViewReport.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewReport_CellClick);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.IndianRed;
+            this.label2.Location = new System.Drawing.Point(14, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(209, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select on sale report to view items";
+            // 
             // ReportForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridViewReport);
             this.Controls.Add(this.label1);
             this.Name = "ReportForm";
@@ -78,5 +91,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridViewReport;
+        private System.Windows.Forms.Label label2;
     }
 }
